2c84e3f15430b417676fc8bbf3e9912eb3ec3a5c
Petra Andersson <76211089+henewastaken@users.noreply.github.com>
2022-05-16 14:30:34 +0300

    Adding react stuff

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import StartScreen from './app/Screens/StartScreen';
+
+export default function App() {
+  return (
+    <StartScreen />
+  );
+}
